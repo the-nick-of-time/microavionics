4f091c3bf5eeb522160d5c202c644e5ca957704f
Nick Thurmes <nthurmes@gmail.com>
2017-11-06 09:32:01 -0700

    commuications API

--- comms.h
+++ comms.h
@@ -1,12 +1,24 @@
 #ifndef COMMUNICATIONS
 #define COMMUNICATIONS
 
+#include "targeting.h"
+#include "bool.h"
+
+// Tries tp a usart communication line to the other board and returns whether it was successful
 bool startup_usart(void);
 
+// switch to transmit mode (your turn) or recieve mode (other person's turn)
+bool tx_mode(void);
+bool rx_mode(void);
+
 // send cell that is being targeted
-void send_target(Target tosend);
+bool send_target(Target tosend);
 
 // recieve cell that is being targeted
-Target recieve_target(void);
+Target receive_target(void);
+
+// Confirmation says that 1) the data was successfully received and 2) whether the shot was a hit or a miss
+bool send_confirmation(Board* board, Target targeted);
+bool receive_confirmation(Board* board, Target targeted);
 
 #endif
