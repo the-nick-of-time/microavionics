fa482cc4894f4abc70ea47211a6f5ce2c53b3dce
Nick Thurmes <nthurmes@gmail.com>
2017-11-07 12:29:33 -0700

    LCD framework and basic documentation

---
+++ lcd.c
@@ -0,0 +1,5 @@
+#include "lcd.h"
+
+/*
+NOTE TO MATT: lcd.h only defines the public-facing functions, you will likely need to define additional helper functions like one to draw one pixel on the GLCD
+*/
--- lcd.h
+++ lcd.h
@@ -3,15 +3,21 @@
 
 #include "targeting.h"
 #include "bool.h"
+#include <delays.h>
 
+// Initialize the LCD, return true when done successfully
 bool startup_lcd(void);
 
+// Initialize the GLCD, return true when done successfully
 bool startup_glcd(void);
 
-bool write_lcd_character(char c, unsigned char cursor);
-
+// Write a string to the LCD starting at the cursor position given
 bool write_lcd_string(char* s, unsigned char cursor);
 
+// Draws the whole board, probably will only be called at the beginning on the empty board
 bool draw_board(Board* board);
+
+// Draws an individual cell, probably called every time a target is chosen
+bool draw_cell(Cell cell, char x, char y);
 
 #endif
