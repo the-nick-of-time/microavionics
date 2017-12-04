471a6d669f0fdd48ee32129e88eb3aeef5839b50
Nick Thurmes <nthurmes@gmail.com>
2017-12-04 15:02:18 -0700

    Implemented target selection on ports H and J

---
+++ targeting.c
@@ -0,0 +1,94 @@
+#include "targeting.h"
+#include "bool.h"
+
+#define ROWPORT PORTH
+#define COLPORT PORTJ
+
+Target determine_target(void) {
+	bool rowfound = false, colfound = false;
+	Target target;
+	while (!rowfound) {
+		while (!ROWPORT) {}
+		switch (ROWPORT) {
+			case 1:
+				target.row = 0;
+				rowfound = true;
+				break;
+			case 2:
+				target.row = 1;
+				rowfound = true;
+				break;
+			case 4:
+				target.row = 2;
+				rowfound = true;
+				break;
+			case 8:
+				target.row = 3;
+				rowfound = true;
+				break;
+			case 16:
+				target.row = 4;
+				rowfound = true;
+				break;
+			case 32:
+				target.row = 5;
+				rowfound = true;
+				break;
+			case 64:
+				target.row = 6;
+				rowfound = true;
+				break;
+			case 128:
+				target.row = 7;
+				rowfound = true;
+				break;
+			default:
+	      // More than one button was pressed
+				// target.error = 0b11;
+				// return target;
+				break;
+		}
+	}
+	while (!colfound) {
+		while (!COLPORT) {}
+		switch (COLPORT) {
+			case 1:
+				target.col = 0;
+				colfound = true;
+				break;
+			case 2:
+				target.col = 1;
+				colfound = true;
+				break;
+			case 4:
+				target.col = 2;
+				colfound = true;
+				break;
+			case 8:
+				target.col = 3;
+				colfound = true;
+				break;
+			case 16:
+				target.col = 4;
+				colfound = true;
+				break;
+			case 32:
+				target.col = 5;
+				colfound = true;
+				break;
+			case 64:
+				target.col = 6;
+				colfound = true;
+				break;
+			case 128:
+				target.col = 7;
+				colfound = true;
+				break;
+			default:
+	      // More than one button was pressed
+				break;
+		}
+	}
+	target.error = 0b00;
+	return target;
+}
