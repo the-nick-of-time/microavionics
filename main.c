e8cde299dc6a146baa9959a43ffc69de95976224
Nick Thurmes <nthurmes@gmail.com>
2017-12-12 14:29:07 -0700

    Debug version of main

--- main.c
+++ main.c
@@ -20,13 +20,20 @@ void main(void) {
 	// keep on trying to start up things until they succeed
 	while (!startup_glcd()){}
 	while (!startup_usart()){}
-	for (i = 0; i < 64; i++) {
-		cell = get_cell(&myBoard, i%WIDTH, i/WIDTH);
-		cell->targeted = true;
-	}
+    TRISJ = 0xFF;
+    TRISF = 0x00;
+    TRISE = 0xFF;
+//	for (i = 0; i < 64; i++) {
+//		cell = get_cell(&myBoard, i%WIDTH, i/WIDTH);
+//		cell->targeted = true;
+//	}
 	// main loop
 	while (1) {
 		draw_board(&myBoard);
+        target = determine_target();
+//        cell = get_cell(&myBoard, target.row, target.col);
+//        cell->targeted = true;
+        send_target(target);
 //		if (mode == MY_TURN) {
 //			target = determine_target();
 //			send_target(target);
