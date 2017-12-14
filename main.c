c285ecb6bb8f4e77b7a40b1e5b3f7398df8c3868
Nick Thurmes <nthurmes@gmail.com>
2017-12-13 18:19:06 -0700

    cell needs to be targeted see lines 46-47

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ void main(void) {
 	Target target;
 	Cell* cell;
 	Board myBoard;
-	char i;
 	char myturn[] = {'M','Y',' ','T','U','R','N','\0'};
 	char theirturn[] = {'T','H','E','I','R',' ','T','U','R','N','\0'};
 	// keep on trying to start up things until they succeed
@@ -24,24 +23,28 @@ void main(void) {
 	TRISF = 0x00;
 	LATF = 0x00;
 	TRISE = 0xFF;
-	mode = MY_TURN;
 	myBoard = create_board();
+    // note: if this moves before "myBoard = create_board();", mode gets corrupted
+    mode = MY_TURN;
 	// main loop
 	while (1) {
 		draw_board(&myBoard);
+        Nop();
 		if (mode == MY_TURN) {
 			LATF = 0xF0;
-			write_string(myturn, 64+7, 3);
+			// write_string(myturn, 64+7, 3);
 			target = determine_target();
 			send_target(target);
 			mode = THEIR_TURN;
 		}
 		else {
 			LATF = 0x0F;
-			write_string(theirturn, 64+7, 3);
+			// write_string(theirturn, 64+7, 3);
 			do {
 				target = receive_target();
 			} while (target.error);
+            cell = get_cell(&myBoard, target.row, target.col);
+            cell->targeted = true;
 			mode = MY_TURN;
 		}
 	}
